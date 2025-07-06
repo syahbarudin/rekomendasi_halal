dd7abe0acb4cf2320372deeba6133f8f32eaa93e
Syahbarudin Abdillah <146621192+syahbarudin@users.noreply.github.com>
2025-07-06 08:49:16 +0700

    Update rekomendasi.py

--- rekomendasi.py
+++ rekomendasi.py
@@ -7,18 +7,30 @@ from collections import Counter
 
 # --- FUNGSI BARU UNTUK IKON GITHUB ---
 def tampilkan_ikon_github():
-    """Menampilkan ikon GitHub yang menarik di pojok kanan bawah."""
+    """Menampilkan ikon GitHub dengan tulisan "Click Here" di atasnya."""
     # GANTI DENGAN URL GITHUB ANDA
-    github_url = "https://github.com/syahbarudin/rekomendasi_halal"
+    github_url = "https://github.com/username/repo"
     
-    # CSS untuk styling dan animasi
+    # CSS untuk styling wadah, teks, dan ikon
     st.markdown(f"""
     <style>
-    #github-link {{
+    #github-container {{
         position: fixed;
         bottom: 25px;
         left: 25px;
         z-index: 1000;
+        text-align: center;
+    }}
+    .github-text {{
+        font-size: 12px;
+        color: grey;
+        margin-bottom: 5px;
+        font-family: sans-serif;
+        text-decoration: none;
+        font-weight: bold; /* Menambahkan tebal agar lebih terlihat */
+    }}
+    #github-link {{
+        display: inline-block;
         transition: transform 0.2s ease-in-out;
     }}
     #github-link:hover {{
@@ -30,13 +42,17 @@ def tampilkan_ikon_github():
     }}
     </style>
     
-    <a href="{github_url}" target="_blank" id="github-link">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="github-svg">
-            <path fill="grey" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-        </svg>
-    </a>
+    <div id="github-container">
+        {/* --- PERUBAHAN DI SINI --- */}
+        <p class="github-text">Click here to view source code </p>
+        
+        <a href="{github_url}" target="_blank" id="github-link">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="github-svg">
+                <path fill="grey" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+            </svg>
+        </a>
+    </div>
     """, unsafe_allow_html=True)
-
 
 @st.cache_data
 def load_and_process_data():
